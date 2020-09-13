adbee1ac3f9e74db84462ed6e4dd862b227cd25b
Mitch Denny <mitch@mitchdenny.com>
2020-09-13 21:04:13 +1000

    Initial Commit

---
+++ SPMInteropApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SPMInteropApp
+//
+//  Created by Mitch Denny on 13/9/20.
+//  Copyright © 2020 Mitch Denny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
